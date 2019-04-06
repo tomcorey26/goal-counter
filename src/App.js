952b75583f1373d2183c25df0ef38ae3b2caf781
tomcorey26 <tomcorey26@gmail.com>
2019-04-06 17:08:30 -0400

    Got bootstrap set up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Year from "./components/Year/Year";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div class="container-fluid">
+        <div class="jumbotron text-center">
+          <h1>My First Bootstrap Page</h1>
+          <p>Resize this responsive page to see the effect!</p>
+        </div>
         <Year />
       </div>
     );
