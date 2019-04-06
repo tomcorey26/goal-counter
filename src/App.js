74fd523cf86bc0100c87083e623cc8b9b23dbd88
tomcorey26 <tomcorey26@gmail.com>
2019-04-06 17:46:37 -0400

    added calendar.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 import Year from "./components/Year/Year";
+import Calendar from "react-calendar";
 
 class App extends Component {
   render() {
+    this.state = {
+      date: new Date()
+    };
+
+    this.onChange = date => this.setState({ date });
     return (
       <div class="container-fluid">
         <div class="jumbotron text-center">
-          <h1>My First Bootstrap Page</h1>
-          <p>Resize this responsive page to see the effect!</p>
+          <h1>Daily Goal Tracker</h1>
         </div>
-        <Year />
+        <Calendar onChange={this.onChange} value={this.state.date} />
       </div>
     );
   }
